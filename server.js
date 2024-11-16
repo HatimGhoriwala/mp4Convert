7eb6ef1d82e799307dfc12b25129aed6b79a75b6
Mr Perfect <hatimghoriwala51@gmail.com>
2024-11-16 19:27:53 +0530

    first commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
 app.use(express.static('public'));
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
